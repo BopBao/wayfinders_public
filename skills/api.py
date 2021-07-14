777d2eea8db57fa1df3020378ff13d10800d8116
caseystrong <kcorionstrong@hotmail.com>
2021-07-14 13:21:32 -0400

    manual api merge

--- skills/api.py
+++ skills/api.py
@@ -1,11 +1,13 @@
 from django.db.models import query
 from rest_framework import viewsets
 from .serializers import SkillSerializer
-from .models import Skill
+from .models import Skill, MemberToSkills, UserToSkills
 from wayfinders.functions import add_to_queryset
 
 import logging
+
 logger = logging.getLogger('skills.api')
+
 
 class SkillAPI(viewsets.ModelViewSet):
     serializer_class = SkillSerializer
@@ -20,11 +22,11 @@ class SkillAPI(viewsets.ModelViewSet):
         add_to_queryset(queryset_params, 'name__icontains', name)
         add_to_queryset(queryset_params, 'description__icontains', description)
 
-        #Strings icontains, Booleans if statement, Integer equals
+        # Strings icontains, Booleans if statement, Integer equals
 
-        if(official == "true"):
+        if (official == "true"):
             add_to_queryset(queryset_params, 'official', True)
-        elif(official == "false"):
+        elif (official == "false"):
             add_to_queryset(queryset_params, 'official', False)
 
         if queryset_params:
@@ -36,10 +38,34 @@ class SkillAPI(viewsets.ModelViewSet):
 class MemberToSkillsAPI(viewsets.ModelViewSet):
     serializer_class = SkillSerializer
     http_method_names = ['get', 'head', 'post', 'put']
-    queryset = Skill.objects.all()
+
+    def get_queryset(self):
+        member_id = self.request.query_params.get('member_id')  # int
+        skill_id = self.request.query_params.get('skill_id')  # int
+
+        queryset_params = {}
+        add_to_queryset(queryset_params, 'member_id', member_id)
+        add_to_queryset(queryset_params, 'skill_id', skill_id)
+
+        if queryset_params:
+            return MemberToSkills.objects.filter(**queryset_params)
+
+        return MemberToSkills.objects.all()
 
 
 class UserToSkillsAPI(viewsets.ModelViewSet):
     serializer_class = SkillSerializer
     http_method_names = ['get', 'head', 'post', 'put']
-    queryset = Skill.objects.all()
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user_id')  # int
+        skill_id = self.request.query_params.get('skill_id')  # int
+
+        queryset_params = {}
+        add_to_queryset(queryset_params, 'user_id', user_id)
+        add_to_queryset(queryset_params, 'skill_id', skill_id)
+
+        if queryset_params:
+            return UserToSkills.objects.filter(**queryset_params)
+
+        return UserToSkills.objects.all()
